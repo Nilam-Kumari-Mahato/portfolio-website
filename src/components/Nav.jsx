2e3ebe098f4dba0bfc90f19645c04498f882a8e4
Nilam Kumari Mahato <nilamkumarimahato10@gmail.com>
2025-06-28 12:20:42 +0530

    making it more responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,7 +8,6 @@ export default function Nav(){
             <a href="#about">About</a>
             <a href="#skills">Skills</a>
             <a href="#projects">Projects</a>
-            <a href="#contact">Contact</a>
         </div>
     )
 }
